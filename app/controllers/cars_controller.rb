83c34c2e8bb5d70549fe34e4ff81469adbc08e12
dani <danifromecuador@gmail.com>
2023-10-29 15:05:05 -0500

    Create view and controller for car new

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,4 +8,24 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
     pp @car
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to cars_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:make, :model, :year, :color)
+  end
 end
