7823538c7d184757a4f7983aa7637c530c4c0e84
dani <danifromecuador@gmail.com>
2023-10-30 13:20:35 -0500

    Create method create for cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -11,4 +11,20 @@ class CarsController < ApplicationController
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      pp @car.errors
+      render 'new'
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :year, :color)
+  end
 end
